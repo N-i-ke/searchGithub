80278c071ee15c86fcddb4a6dc45a615df9c1cb3
N-i-ke <nike.forgithub@gmail.com>
2024-01-24 00:01:53 +0900

    ユーザーネーム検索機能

--- script.js
+++ script.js
@@ -1,41 +1,59 @@
-const button = document.getElementById("click");
+const searchButton = document.getElementById("search");
 const clearButton = document.getElementById("clear");
 const type = document.getElementById("type");
-let isFetching = false;
+const inputUser = document.querySelector("input[name=username]");
+const msg = document.getElementById("msg");
 
-button.addEventListener("click", () => {
+let isFetching = false;
+let newElement;
+
+searchButton.addEventListener("click", () => {
+
+  alert(inputUser.value)
+  msg.innerText = inputUser.value
+  
+  //inputに入力された値 
+  var userValue = inputUser.value;
+  console.log(userValue)
+
+
   if (isFetching) {
     console.log("Fetching True");
     return;
   }
-    
-  isFetching = true;
 
-  let url = "https://api.github.com/users/N-i-ke/repos";
-    
+  isFetching = true;
+  let url = `https://api.github.com/users/${userValue}/repos`;
+  console.log(url)
+
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data)  
-      for (let i = 0; i < data.length; i++) {
-        newElement = document.createElement("div");
-        newElement.innerHTML = data[i].name;
-        type.appendChild(newElement);
-        console.log(data[i].name);
-      }
+      console.log(data);
+      displayRepositories(data);
+    })
+    .finally(() => {
+      isFetching = false;
     });
 });
 
 clearButton.addEventListener("click", () => {
-  // <div>内のすべての子要素を削除
+  clearRepositories();
+});
+
+function displayRepositories(data) {
+  clearRepositories();
+  for (let i = 0; i < data.length; i++) {
+    newElement = document.createElement("div");
+    newElement.innerHTML = data[i].name;
+    type.appendChild(newElement);
+    console.log(data[i].name);
+  }
+}
+
+function clearRepositories() {
   while (type.firstChild) {
     type.removeChild(type.firstChild);
   }
-  //通信を切る  
-  if (isFetching) {
-    console.log("Fetching False");
-    isFetching = false;
-    return;
-  }
+}
 
-});
